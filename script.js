8661f3164d0b52ed8127c3c44132c0d577841418
Maxime Paolone <mxpln@outlook.com>
2020-03-25 18:56:46 +0100

    Iteration 1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // ITERATION 1: The Fortune Teller
-
-
+function tellFortune(numChildren, partnerName, geoLoc, jobTitle) {
+  return `You will be a ${jobTitle} in ${geoLoc} and married to ${partnerName} with ${numChildren} kids`;
+}
+console.log(tellFortune(2, "Julie", "Paris", "full stack developer"));
 // ITERATION 2: The Puppy Age Calculator
 
-
 // ITERATION 3: The Geometrizer
-
 
 // ITERATION 4: The Temperature Converter
