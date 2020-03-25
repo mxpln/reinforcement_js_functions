ff4c973f4cfaf904deab0373b9322a7cb0844ded
Maxime Paolone <mxpln@outlook.com>
2020-03-25 19:38:30 +0100

    End of reinforcement

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ function tellFortune(numChildren, partnerName, geoLoc, jobTitle) {
   return `You will be a ${jobTitle} in ${geoLoc} and married to ${partnerName} with ${numChildren} kids`;
 }
 console.log(tellFortune(2, "Julie", "Paris", "full stack developer"));
-// ITERATION 2: The Puppy Age Calculator
+// ITERATION 2: Display the squared values of the numbers
+
+function squared(age) {
+  for (let i = 0; i <= age; i++) {
+    console.log(i ** 2);
+  }
+}
+squared(12);
+
+// FOR () : you know the number of iterations
+
+// WHILE (trueCondition) : you don't
 
 // ITERATION 3: The Geometrizer
 
